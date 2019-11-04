a20e2610fd541417e1a347df1717e3707674af96
MontySteele <montgomerysteele@gmail.com>
2019-11-04 16:42:35 -0500

    Update api.py

--- Notebooks/api.py
+++ Notebooks/api.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+# handle imports
+
 from fastai.text import *
 from fastai.basics import *
 import json
 
+#load the model
+
 path = r'/home/montgomerysteele/nlp_data_msteele/Notebooks'
 learn = load_learner(path, 'trained_model.pkl')
+
+# Define my predictor function, which takes in a comment and returns the prediction for the number of 'likes
 
 def predictor(test_comment):
         
@@ -22,10 +28,13 @@ def predictor(test_comment):
         print('This comment was placed in category ' + category + '. This means that we predict your comment will have between ' + leftBin + ' and ' + rightBin + ' recommendations.')
         return(category, leftBin, rightBin)
 
-  
+# set up the homepage
+        
 @app.route('/', methods=['GET'])    
 def home():
     return render_template('home.html')    
+
+# Handle api calls
     
 @app.route('/comment/page/',methods=['GET', 'POST'])
 def server():
@@ -40,6 +49,9 @@ def server():
     else:
         return '''<h1>Comment Quality Analyzer</h1>
 <p>This is an error page. Did you type the link in correctly?</p>''' 
+
+
+@ Handle webpage requests
 
 @app.route('/comment/api/',methods=['GET', 'POST'])
 def webRequest():
